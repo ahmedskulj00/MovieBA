49b07a767417126ed9ae71704af6b9399eed8d16
ahmedskulj <ahmedskulj10@gmail.com>
2022-06-06 15:44:18 +0200

    Installing all necessary dependecies.

---
+++ src/lib/Firebase/firebase_cfg.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA5z1wV9CISnSM_B8NgYO0sRckzAShurCY",
+  authDomain: "graduation-movie-app.firebaseapp.com",
+  projectId: "graduation-movie-app",
+  storageBucket: "graduation-movie-app.appspot.com",
+  messagingSenderId: "764688283212",
+  appId: "1:764688283212:web:96a6d9f6d1c5298227a6e9",
+  measurementId: "G-G8TYHH2KJF",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
